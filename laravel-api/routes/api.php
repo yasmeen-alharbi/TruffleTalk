3249e8e534b5d7754e1a546a773d27e0b3ec753f
Jordyn Espenant <93738138+jordynesp@users.noreply.github.com>
2023-11-06 01:19:05 -0600

    Started adding Sanctum logic for authentication

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/token', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
