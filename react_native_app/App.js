9395f2cb23862d7dd73fe5e696215080fadc88da
Yasmeen Al-Harbi <yaa300@usask.ca>
2023-11-11 01:23:57 -0600

    Cleans up app.js

--- react_native_app/App.js
+++ react_native_app/App.js
@@ -1,17 +1,3 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, View } from 'react-native';
-// // import { config } from '@gluestack-ui/config'; // Optional if you want to use default theme
-
-// export default function App() {
-//   return (
-//     <NativeBaseProvider>
-//       <View style={styles.container}>
-//         <Text>Open up App.js to start working on your app!</Text>
-//         <StatusBar style="auto" />
-//       </View>
-//     </NativeBaseProvider>
-//   );
-// }
 import React from "react";
 import { NativeBaseProvider, Box } from "native-base";
 
