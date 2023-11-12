8ece985c2fa06541b2f63b7f1c87db6c0e53f63b
Yasmeen Al-Harbi <yaa300@usask.ca>
2023-11-11 18:53:49 -0600

    fixes start up view styling

--- react_native_app/src/StartUp.js
+++ react_native_app/src/StartUp.js
@@ -8,41 +8,37 @@ import {
     Column,
     View,
     Button,
-    Link
+    Link,
+    Spacer
 } from  'native-base'
 
 const StartUp = () => {
     return (
-        <View width="100%" height="100%">
-            <Column alignItems="center" mt="20">
-                <Center>
-                    <Image style={{ width: 51, height: 51 }} source={require('./mushroom.png')} alt='Alt text'/>
-                    <Container mt="5">
-                        <Heading>
-                            Welcome to
-                            <Text color="primary.400"> TruffleTalk </Text>
-                        </Heading>
-                    </Container>
-                    <Container>
-                        <Text mt="3" fontWeight="medium">
-                            TruffleTalk is an app
-                        </Text>
-                    </Container>
-                    <Button mt="20" borderRadius="full"  shadow="5" variant="subtle" alignContent="center">
-                        <Text ml="8" mr="8" fontWeight="medium">
-                            SIGN UP
-                        </Text>
-                    </Button>
-                    <Button  mt="5" borderRadius="full"  shadow="5" alignContent="center">
-                        <Text ml="10" mr="10" fontWeight="medium" color="white">
-                            LOGIN
-                        </Text>
-                    </Button>
-                    <Text mt="5"> 
-                    Just browsing? <Link>Continue as guest</Link>
+        <View width="100%" height="100%" pt="20" alignItems="center">
+            <Center mt="5">
+                <Image style={{ width: 70, height: 70 }} source={require('./mushroom.png')} alt='Alt text'/>
+                <Heading size="3xl" color="primary.400">
+                    TruffleTalk
+                </Heading>
+                <Text fontWeight="medium" bold>
+                    Where Myco-Minds Meet!
+                </Text>
+            </Center>
+            <Center pt="20">
+                <Button borderRadius="full"  shadow="5" variant="subtle" alignContent="center">
+                    <Text ml="8" mr="8" fontWeight="medium">
+                        SIGN UP
                     </Text>
-                </Center>
-            </Column>
+                </Button>
+                <Button mt="5" borderRadius="full"  shadow="5" alignContent="center">
+                    <Text ml="10" mr="10" fontWeight="medium" color="white">
+                        LOGIN
+                    </Text>
+                </Button>
+                <Text mt="5"> 
+                    Just browsing? <Link _text={{color: "primary.600"}}>Continue as guest</Link>
+                </Text>
+            </Center>
         </View>
     );
 };
