c4dbf14f91bff6747e5a2a0d09f3ec76cc588058
Jordyn Espenant <93738138+jordynesp@users.noreply.github.com>
2023-11-12 23:13:19 -0600

    Updated store method for Posts to handle validation and file storage

--- laravel-api/app/Http/Controllers/PostController.php
+++ laravel-api/app/Http/Controllers/PostController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    /**
+     * Creates a post.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $data = $request->validate([
+            'title' => 'required|string|max:255',
+            'mushroom' => 'required|string|max:255',
+            'description' => 'required|string',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('uploads/posts', 'public');
+            $data['image'] = $imagePath;
+        }
+
+        $post = new Post($data);
+        $post->user_id = $request->user()->id;
+        $post->save();
+
+        return response()->json(['post' => new PostResource($post)], 201);
+    }
 }
