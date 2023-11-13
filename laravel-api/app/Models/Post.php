393dbc5713b9064147f64b9efe82b1a91ccd101d
Jordyn Espenant <93738138+jordynesp@users.noreply.github.com>
2023-11-12 22:58:27 -0600

    Added fillable and guarded properties to Post model

--- laravel-api/app/Models/Post.php
+++ laravel-api/app/Models/Post.php
@@ -4,8 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'mushroom',
+        'description',
+        'image',
+    ];
+
+    /**
+     * The attributes that are not mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $guarded = [
+        'user_id',
+    ];
+
+    /**
+     * Get the author of the post.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
