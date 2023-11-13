e2f4b50a44b763526be0cab4281d6f0a331bae1e
Jordyn Espenant <93738138+jordynesp@users.noreply.github.com>
2023-11-13 00:25:57 -0600

    Added PostFactory and new posts to seeder

--- laravel-api/database/seeders/DatabaseSeeder.php
+++ laravel-api/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -19,5 +19,7 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
             'email_verified_at' => null,
         ]);
+
+        Post::factory()->count(5)->create();
     }
 }
