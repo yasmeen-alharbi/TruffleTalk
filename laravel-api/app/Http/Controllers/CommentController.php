3b4243dae6925ba27e89699f364176f04e6560ec
Jordyn Espenant <93738138+jordynesp@users.noreply.github.com>
2023-11-20 01:21:12 -0600

    Added constructor for comment controller

--- laravel-api/app/Http/Controllers/CommentController.php
+++ laravel-api/app/Http/Controllers/CommentController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    /**
+     * CommentController constructor.
+     *
+     * @param Post $post
+     */
+    public function __construct(protected Post $post) {}
 }
