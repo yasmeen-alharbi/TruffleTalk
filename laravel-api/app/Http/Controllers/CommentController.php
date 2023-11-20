54779c4bb45d2891163101ee2bd80576ea5630cf
Jordyn Espenant <93738138+jordynesp@users.noreply.github.com>
2023-11-20 01:39:42 -0600

    Added store comment method in comment controller

--- laravel-api/app/Http/Controllers/CommentController.php
+++ laravel-api/app/Http/Controllers/CommentController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostResource;
 use App\Models\Post;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
@@ -13,4 +15,31 @@ class CommentController extends Controller
      * @param Post $post
      */
     public function __construct(protected Post $post) {}
+
+    /**
+     * Creates a user's comment for a post.
+     *
+     * @param Request $request
+     * @param int $postId
+     * @return JsonResponse
+     */
+    public function store(Request $request, int $postId): JsonResponse
+    {
+        $post = $this->post->newQuery()->where('id', $postId)->first();
+
+        if (!$post) {
+            return response()->json(['message' => 'Post not found'], 404);
+        }
+
+        $data = $request->validate([
+            'content' => 'required|string|max:255',
+        ]);
+
+        $post->comments()->create([
+            'user_id' => $request->user()->id,
+            'content' => $data['content'],
+        ]);
+
+        return response()->json(['post' => new PostResource($post)], 201);
+    }
 }
