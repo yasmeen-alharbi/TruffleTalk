5a01f5aebf72fec23296fe91fa94360f73ab342b
Jordyn Espenant <93738138+jordynesp@users.noreply.github.com>
2023-11-21 16:20:46 -0600

    Added endpoints and controller methods to get followed posts and recommended posts

--- laravel-api/app/Http/Controllers/PostController.php
+++ laravel-api/app/Http/Controllers/PostController.php
@@ -47,15 +47,67 @@ class PostController extends Controller
     }
 
     /**
-     * Gets all the posts.
+     * Gets all the posts, sorted by top-rated post.
+     * Will be used by guests.
      *
      * @return AnonymousResourceCollection
      */
     public function index(): AnonymousResourceCollection
     {
-        $posts = $this->post->with(['comments', 'user:id,username'])->get();
+        $posts = $this->post->newQuery()
+            ->with(['comments', 'user:id,username'])
+            ->withCount(['likes', 'comments'])
+            ->orderByRaw('likes_count + comments_count DESC')
+            ->get();
 
         return PostResource::collection($posts);
+    }
+
+    /**
+     * Gets all user's followed posts, sorted chronologically.
+     *
+     * @param Request $request
+     * @return AnonymousResourceCollection
+     */
+    public function followed(Request $request): AnonymousResourceCollection
+    {
+        $currentUser = $request->user();
+
+        $followingIds = $currentUser->following()->pluck('followed_id')->toArray();
+        $followingIds[] = $currentUser->id;
+
+        $followedPosts = $this->post->newQuery()
+            ->with(['comments', 'user:id,username'])
+            ->whereIn('user_id', $followingIds)
+            ->orderBy('created_at','DESC')
+            ->get();
+
+        return PostResource::collection($followedPosts);
+    }
+
+    /**
+     * Gets recommended posts from users that the user is not
+     * following, sorts by top-rated.
+     *
+     * @param Request $request
+     * @return AnonymousResourceCollection
+     */
+    public function recommended(Request $request): AnonymousResourceCollection
+    {
+        $currentUser = $request->user();
+
+        $followingIds = $currentUser->following()->pluck('followed_id')->toArray();
+        $followedPostsIds = $this->post->newQuery()->whereIn('user_id', $followingIds)->pluck('id')->toArray();
+
+        $recommendedPosts = $this->post->newQuery()
+            ->with(['comments', 'user:id,username'])
+            ->whereNotIn('user_id', [$currentUser->id])
+            ->whereNotIn('id', $followedPostsIds)
+            ->withCount(['likes', 'comments'])
+            ->orderByRaw('(likes_count + (3 * comments_count)) DESC')
+            ->get();
+
+        return PostResource::collection($recommendedPosts);
     }
 
     /**
