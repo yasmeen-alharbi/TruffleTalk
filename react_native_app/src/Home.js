85b4ec9fac6d3ed5382bd696246112e8387fabf6
Yasmeen Al-Harbi <yaa300@usask.ca>
2023-11-21 00:29:14 -0600

    Adds like button to post and polish

--- react_native_app/src/Home.js
+++ react_native_app/src/Home.js
@@ -9,7 +9,6 @@ import {
     Spinner,
     Heading,
     Divider,
-    Container,
     ScrollView,
 } from  'native-base';
 import { Image } from 'react-native';
@@ -21,9 +20,15 @@ import AppHeader from './Components/AppHeader';
 
 const Home = () => {
     const [loading, setLoading] = useState(false);
+    const [like, setLike] = useState(false);
+    
     const showComments = () => {
         console.log("lol");
-    }
+    };
+
+    const likePost = () => {
+        setLike(!like);
+    };
 
     return (
         <View>
@@ -44,14 +49,14 @@ const Home = () => {
                             <HStack pl="3" pt="3" pb="1" justifyContent="space-between" pr="3">
                                 <VStack>
                                     <Text fontSize="lg" bold>
-                                        Username
+                                        @username
                                     </Text>
                                     <Text fontSize="xs" italic>
                                         mushroom
                                     </Text>
                                 </VStack>
                                 <Button size={8} alignItems="center">
-                                    <Icon as={<Ionicons name="add"/>} color="primary.100"/>
+                                    <Icon as={<Ionicons name="add"/>} size="5" color="primary.50"/>
                                 </Button>
                             </HStack>
                             <Divider bg="blueGray.200" shadow="3"/>
@@ -61,21 +66,24 @@ const Home = () => {
                             <Divider bg="blueGray.200"/>
                             <VStack pl="3" pt="3" pb="1">
                                 <HStack justifyContent="space-between" pr="3">
-                                    <Text fontSize="md">
+                                    <Text fontSize="md" bold>
                                         Title
                                     </Text>
                                     <Text fontSize="sm" color="muted.500">
                                         19/14/22
                                     </Text>
                                 </HStack>
-                                <Container>
+                                <VStack>
                                     <Text>
                                         this is my cool ass mushroom :3 whoah look at how ocook r oig  uerbigujnrgubiuvbiub
                                     </Text>
-                                    <Text pt="2" fontSize="xs" color="muted.500" onPress={showComments}>
-                                        11 comments...
-                                    </Text>
-                                </Container>
+                                    <HStack justifyContent="space-between" pr="3">
+                                        <Text pt="2" fontSize="xs" color="muted.500" onPress={showComments}>
+                                            11 comments...
+                                        </Text>
+                                        <Ionicons name={`${like ? "heart-outline" : "heart-sharp"}`} size={24} color={`${like ? "black" : "red"}`} onPress={likePost}/>
+                                    </HStack>
+                                </VStack>
                             </VStack>
                             <Divider bg="blueGray.200"/>
                         </VStack>
