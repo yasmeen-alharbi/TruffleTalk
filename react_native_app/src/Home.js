a7273f426751f49f03f8234f80ce97025feadda0
Yasmeen Al-Harbi <yaa300@usask.ca>
2023-11-20 23:36:55 -0600

    Adds a loading state

--- react_native_app/src/Home.js
+++ react_native_app/src/Home.js
@@ -6,66 +6,82 @@ import {
     VStack,
     HStack,
     Button,
+    Spinner,
+    Heading,
     Divider,
     Container,
     ScrollView,
 } from  'native-base';
-import React from 'react';
 import { Image } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import React, { useState } from 'react';
 import { Dimensions } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 import AppHeader from './Components/AppHeader';
 
 const Home = () => {
+    const [loading, setLoading] = useState(false);
     const showComments = () => {
         console.log("lol");
     }
 
     return (
         <View>
-            <AppHeader />
-            <ScrollView w="100%" h="100%">
-                <VStack>
-                    <HStack pl="3" pt="3" pb="1" justifyContent="space-between" pr="3">
-                        <VStack>
-                            <Text fontSize="lg" bold>
-                                Username
-                            </Text>
-                            <Text fontSize="xs" italic>
-                                mushroom
-                            </Text>
-                        </VStack>
-                        <Button size={8} alignItems="center">
-                            <Icon as={<Ionicons name="add"/>} color="primary.100"/>
-                        </Button>
+            {loading ? (
+                <VStack justifyContent="center" h="100%">
+                    <HStack space={2} justifyContent="center">
+                        <Spinner />
+                        <Heading color="primary.500" fontSize="md">
+                            Loading
+                        </Heading>
                     </HStack>
-                    <Divider bg="blueGray.200" shadow="3"/>
-                    <Box>
-                        <Image style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').width }} source={require('./mushroom.png')} alt='Alt text'/>
-                    </Box>
-                    <Divider bg="blueGray.200"/>
-                    <VStack pl="3" pt="3" pb="1">
-                        <HStack justifyContent="space-between" pr="3">
-                            <Text fontSize="md">
-                                Title
-                            </Text>
-                            <Text fontSize="sm" color="muted.500">
-                                19/14/22
-                            </Text>
-                        </HStack>
-                        <Container>
-                            <Text>
-                                this is my cool ass mushroom :3 whoah look at how ocook r oig  uerbigujnrgubiuvbiub
-                            </Text>
-                            <Text pt="2" fontSize="xs" color="muted.500" onPress={showComments}>
-                                11 comments...
-                            </Text>
-                        </Container>
-                    </VStack>
-                    <Divider bg="blueGray.200"/>
                 </VStack>
-            </ScrollView>
+            ) : (
+                <>
+                    <AppHeader />
+                    <ScrollView w="100%" h="100%">
+                        <VStack>
+                            <HStack pl="3" pt="3" pb="1" justifyContent="space-between" pr="3">
+                                <VStack>
+                                    <Text fontSize="lg" bold>
+                                        Username
+                                    </Text>
+                                    <Text fontSize="xs" italic>
+                                        mushroom
+                                    </Text>
+                                </VStack>
+                                <Button size={8} alignItems="center">
+                                    <Icon as={<Ionicons name="add"/>} color="primary.100"/>
+                                </Button>
+                            </HStack>
+                            <Divider bg="blueGray.200" shadow="3"/>
+                            <Box>
+                                <Image style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').width }} source={require('./mushroom.png')} alt='Alt text'/>
+                            </Box>
+                            <Divider bg="blueGray.200"/>
+                            <VStack pl="3" pt="3" pb="1">
+                                <HStack justifyContent="space-between" pr="3">
+                                    <Text fontSize="md">
+                                        Title
+                                    </Text>
+                                    <Text fontSize="sm" color="muted.500">
+                                        19/14/22
+                                    </Text>
+                                </HStack>
+                                <Container>
+                                    <Text>
+                                        this is my cool ass mushroom :3 whoah look at how ocook r oig  uerbigujnrgubiuvbiub
+                                    </Text>
+                                    <Text pt="2" fontSize="xs" color="muted.500" onPress={showComments}>
+                                        11 comments...
+                                    </Text>
+                                </Container>
+                            </VStack>
+                            <Divider bg="blueGray.200"/>
+                        </VStack>
+                    </ScrollView>
+                </>
+            )}
         </View>
     );
 };
