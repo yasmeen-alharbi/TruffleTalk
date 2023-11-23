b7513e2218f44797d39b5ecc14b899e7fab858ec
Yasmeen Al-Harbi <yaa300@usask.ca>
2023-11-23 14:41:00 -0600

    Adds right padding to section under post image

--- react_native_app/src/Components/Post.js
+++ react_native_app/src/Components/Post.js
@@ -45,8 +45,8 @@ const Post = ({ data, likePost, showComments }) => {
                     <Image style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').width }} source={{ uri: data.image }} alt='Alt text'/>
                 </Box>
                 <Divider bg="blueGray.200"/>
-                <VStack pl="3" pt="3" pb="2">
-                    <HStack justifyContent="space-between" pr="3">
+                <VStack pl="3" pt="3" pb="2" pr="3">
+                    <HStack justifyContent="space-between">
                         <Text fontSize="md" bold>
                             { data.title }
                         </Text>
@@ -58,7 +58,7 @@ const Post = ({ data, likePost, showComments }) => {
                         <Text>
                             { data.description }
                         </Text>
-                        <HStack justifyContent="space-between" pr="3">
+                        <HStack justifyContent="space-between">
                             <Text pt="2" fontSize="xs" color="muted.500" onPress={showComments}>
                                 {`${data.comments.length} ${data.comments.length === 1 ? "comment" : "comments"}...`}
                             </Text>
