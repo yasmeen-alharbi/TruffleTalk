f3cb47051194dbceb0b74d6b591946e087e6beef
Yasmeen Al-Harbi <yaa300@usask.ca>
2023-11-22 23:42:16 -0600

    Introduces new apis with followed, recommended, and guest feeds

--- react_native_app/src/Home.js
+++ react_native_app/src/Home.js
@@ -1,16 +1,21 @@
 import React, {
+    useRef,
     useState,
     useEffect,
     useContext,
+    useCallback,
 } from 'react';
 import {
     View,
+    Text,
     VStack,
     HStack,
+    Center,
     Spinner,
     Heading,
+    Divider,
     ScrollView,
-} from  'native-base';
+} from 'native-base';
 
 import api from './util/api';
 import Post from './Components/Post';
@@ -24,21 +29,45 @@ const Home = () => {
     const [data, setData] = useState({});
     const [loading, setLoading] = useState(true);
 
+    const scrollRef = useRef(true);
+    const [showRecommended, setShowRecommended] = useState(false);
+    const [recommendedData, setRecommendedData] = useState({});
+    const [recommendedLoading, setRecommendedLoading] = useState(true);
+
     useEffect(() => {
-        api({ token: user.token }).get('/posts')
-        .then(({ data }) => {
-            setData(data.data);
-            setLoading(false);
-        }).catch((errors) => {
-            console.error(errors);
-            setLoading(false);
-        });
+        if (user) {
+            api({ token: user.token }).get('/followed/posts')
+                .then(({ data }) => {
+                    if (data.data.length === 0) { // No posts to catch up on, then immediately load recommended data.
+                        handleScroll();
+                    }
+                    setData(data.data);
+                    setLoading(false);
+                })
+                .catch((errors) => {
+                    console.error(errors);
+                    setLoading(false);
+                });
+        } else {
+            api().get('/posts')
+                .then(({ data }) => {
+                    setData(data.data);
+                    setLoading(false);
+                })
+                .catch((errors) => {
+                    console.error(errors);
+                    setLoading(false);
+                })
+        }
     }, []);
     
     const showComments = () => {
-        console.log("show comments");
+        console.log("show comments"); // TODO
     };
 
+    /**
+     * Used with '/followed' data.
+     */
     const likePost = (postID) => {
         const post = data.filter(obj => {
             return obj.id === postID;
@@ -72,6 +101,72 @@ const Home = () => {
         }
     };
 
+    /**
+     * Used with '/recommended' data.
+     * Need this because we store total page data into two different states for followed and recommended posts.
+     * Without this, states for recommended data won't update and thus like buttons won't update.
+     */
+    const likeRecommendedPost = (postID) => {
+        const post = recommendedData.filter(obj => {
+            return obj.id === postID;
+        });
+
+        if (!post[0].liked_by_current_user) {
+            api({ token: user.token }).post(`/posts/${postID}/likes`)
+                .then(() => {
+                    setRecommendedData(recommendedData.map((prevData) =>
+                        prevData.id === postID
+                            ? {...prevData, liked_by_current_user: true, likes_count: prevData.likes_count + 1}
+                            : prevData
+                    ));
+                })
+                .catch(error => {
+                    console.error(error);
+                })
+        }
+        else {
+            api({ token: user.token }).delete(`/posts/${postID}/likes`)
+                .then(() => {
+                    setRecommendedData(recommendedData.map((prevData) =>
+                        prevData.id === postID
+                            ? {...prevData, liked_by_current_user: false, likes_count: prevData.likes_count - 1}
+                            : prevData
+                    ));
+                })
+                .catch(error => {
+                    console.error(error);
+                })
+        }
+    };
+
+    /**
+     * Checks whether scroll position is close to the bottom of the view.
+     * Used to determine the state of the recommended feed.
+     */
+    const isCloseToBottom = ({ layoutMeasurement, contentOffset, contentSize }) => {
+        const paddingToBottom = 20;
+        return (
+            layoutMeasurement.height + contentOffset.y >=
+            contentSize.height - paddingToBottom
+        );
+    };
+
+    const handleScroll = useCallback(async () => {
+        if (user) {
+            setShowRecommended(true);
+
+            api({token: user.token}).get('/recommended/posts')
+                .then(({data}) => {
+                    setRecommendedData(data.data);
+                    setRecommendedLoading(false);
+                })
+                .catch((error) => {
+                    setRecommendedLoading(false);
+                    console.error(error);
+                });
+        }
+    });
+
     return (
         <View h="100%">
             {loading ? (
@@ -86,10 +181,37 @@ const Home = () => {
             ) : (
                 <>
                     <AppHeader />
-                    <ScrollView w="100%" h="100%">
+                    <ScrollView w="100%" h="100%" onScroll={({ nativeEvent}) => {
+                        if (isCloseToBottom(nativeEvent) && scrollRef.current) {
+                            scrollRef.current = false; // Fetch recommended posts once.
+                            handleScroll();
+                        }
+                    }} scrollEventThrottle={2}>
                         {data.map((data) => (
                             <Post key={ data.id } data={ data } likePost={ () => likePost(data.id) } showComments={ showComments } />
                         ))}
+                        {(showRecommended || data.length === 0) && user ? (
+                            <>
+                            <Center justifyContent="space-between" pb="3" pt="2">
+                                <Text fontSize="md" color="primary.700" bold>
+                                    You're all caught up!
+                                </Text>
+                                <Text fontSize="sm" color="primary.700" bold>
+                                    Here are some posts we think you'll like :)
+                                </Text>
+                            </Center>
+                            <Divider bg="blueGray.200"/>
+                                {recommendedLoading ? (
+                                <Spinner pb="2" pt="2"/>
+                            ) : (
+                                <>
+                                    {recommendedData.map((data) => (
+                                        <Post key={ data.id } data={ data } likePost={ () => likeRecommendedPost(data.id) } showComments={ showComments } />
+                                    ))}
+                                </>
+                            )}
+                            </>
+                        ) : null }
                     </ScrollView>
                     <AppFooter />
                 </>
