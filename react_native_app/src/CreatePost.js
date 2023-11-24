23887dbbcf9c64ab6eec51dad2ca773f7bc0e6c4
Yasmeen Al-Harbi <yaa300@usask.ca>
2023-11-23 21:49:36 -0600

    Uses state to store form data

--- react_native_app/src/CreatePost.js
+++ react_native_app/src/CreatePost.js
@@ -1,3 +1,7 @@
+import React, {
+    useState,
+    useCallback,
+} from 'react';
 import {
     Box,
     Text,
@@ -14,7 +18,6 @@ import {
     FormControl,
 } from 'native-base';
 import { Image } from 'react-native';
-import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as ImagePicker from 'expo-image-picker';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
@@ -28,8 +31,18 @@ const CreatePost = () => {
 
     const navigate = useNavigate();
 
-    const [selectedImage, setSelectedImage] = useState(null);
-    const [selectedMushroom, setSelectedMushroom] = useState('');
+    const [formData, setData] = useState({
+        title: null,
+        mushroom: '',
+        description: null,
+        image: '',
+    });
+    const [error, setError] = useState({
+        title: null,
+        mushroom: null,
+        description: null,
+        image: null,
+    });
 
     const goBack = () => { navigate('/home'); };
 
@@ -43,9 +56,32 @@ const CreatePost = () => {
         });
 
         if (!result.canceled) {
-            setSelectedImage(result.assets[0].uri);
+            setData({ ...formData, image: result.assets[0].uri });
         }
     }
+
+    const onChange = (field, value) => {
+        if (field === 'title') {
+            setError({ ...error, title: null });
+            setData({ ...formData, title: value });
+        }
+        else if (field === 'mushroom') {
+            setError({ ...error, mushroom: null });
+            setData({ ...formData, mushroom: value });
+        }
+        else if (field === 'description') {
+            setError({ ...error, description: null });
+            setData({ ...formData, description: value });
+        }
+        else if (field === 'image') {
+            setError({ ...error, image: null });
+            setData({ ...formData, image: value });
+        }
+    };
+
+    const submit = useCallback(async () => {
+        console.log(formData);
+    });
 
     return (
       <View h="100%">
@@ -60,16 +96,16 @@ const CreatePost = () => {
                           <FormControl.Label>
                               Title
                           </FormControl.Label>
-                          <Input w="64"/>
+                          <Input w="64" onChangeText={value => onChange('title', value)}/>
                       </FormControl>
                       <FormControl pt="3">
                           <FormControl.Label>
                               Mushroom
                           </FormControl.Label>
                           {/* TODO: make dropdown scrollable using a different component */}
-                          <Select w="64" selectedValue={selectedMushroom} placeholder="Choose a Mushroom" _selectedItem={{
+                          <Select w="64" selectedValue={formData.mushroom} placeholder="Choose a Mushroom" _selectedItem={{
                               endIcon: <CheckIcon />
-                          }} onValueChange={value => setSelectedMushroom(value)} _actionSheetBody={{ scrollEnabled: false }}>
+                          }} onValueChange={value => onChange('mushroom', value)} _actionSheetBody={{ scrollEnabled: false }}>
                               {MUSHROOMS.map((mushroom, index) => (
                                   <Select.Item key={index} label={mushroom} value={mushroom} />
                               ))}
@@ -79,11 +115,11 @@ const CreatePost = () => {
                           <FormControl.Label>
                               Description
                           </FormControl.Label>
-                          <TextArea w="64"/>
+                          <TextArea w="64" onChangeText={value => onChange('description', value)}/>
                       </FormControl>
                       <HStack justifyContent="space-between" alignContent="center" w="64" pt="3">
                           <FormControl.Label>
-                              { !selectedImage ? "Upload an Image" : "Choose a different image "}
+                              { formData.image === '' ? "Upload an Image" : "Choose a different image "}
                           </FormControl.Label>
                           <Button _text={{fontWeight:"medium"}} borderRadius="full" shadow="5" onPress={openPhotos}>
                               Upload
@@ -93,7 +129,7 @@ const CreatePost = () => {
               </Center>
               <HStack justifyContent="center" alignContent="center">
                   <VStack justifyContent="space-between" alignContent="center" pt="3" space="3">
-                      { selectedImage ? (
+                      { formData.image !== '' ? (
                           <>
                               <HStack justifyContent="center" alignContent="center">
                                   <Text>
@@ -101,11 +137,11 @@ const CreatePost = () => {
                                   </Text>
                               </HStack>
                               <Box w="40" h="40" rounded="lg" overflow="hidden" borderColor="coolGray.200" borderWidth="1" bg="gray.50" mb="3">
-                                  <Image width="100%" height="100%" resizeMode="contain" source={{ uri: selectedImage }} alt='Alt text'/>
+                                  <Image width="100%" height="100%" resizeMode="contain" source={{ uri: formData.image }} alt='Alt text'/>
                               </Box>
                           </>
                       ) : null }
-                      <Button _text={{fontWeight:"medium"}} borderRadius="full" w="40" shadow="5" mb="3">
+                      <Button _text={{fontWeight:"medium"}} borderRadius="full" w="40" shadow="5" onPress={submit} mb="3">
                           SUBMIT
                       </Button>
                       <Button  borderRadius="full" shadow="5" w="40" variant="subtle" onPress={goBack} mb="5">
