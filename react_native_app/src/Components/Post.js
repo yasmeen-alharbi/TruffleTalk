744b6eeb4952422cf746241a8263750f41779443
Yasmeen Al-Harbi <yaa300@usask.ca>
2023-11-24 14:11:13 -0600

    Changes home post image styling to match preview styling in create post view

--- react_native_app/src/Components/Post.js
+++ react_native_app/src/Components/Post.js
@@ -41,8 +41,8 @@ const Post = ({ data, likePost, showComments }) => {
                     ) : null}
                 </HStack>
                 <Divider bg="blueGray.200"/>
-                <Box bg="gray.100">
-                    <Image style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').width }} resizeMode="contain" source={{ uri: data.image }} alt='Alt text'/>
+                <Box w={ Dimensions.get('window').width } h={ Dimensions.get('window').width } bg="gray.100">
+                    <Image width="100%" height="100%" resizeMode="contain" source={{ uri: data.image }} alt='Alt text'/>
                 </Box>
                 <Divider bg="blueGray.200"/>
                 <VStack pl="3" pt="3" pb="2" pr="3">
