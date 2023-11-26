a0eeb21a030a984861fe0e6534ee3f346f702790
Jordyn Espenant <93738138+jordynesp@users.noreply.github.com>
2023-11-26 02:43:27 -0600

    Used state for search term and moved debounce call to inside of useEffect

--- react_native_app/src/UserSearch.js
+++ react_native_app/src/UserSearch.js
@@ -14,7 +14,7 @@ import {
 import { debounce } from 'lodash';
 import { Dimensions } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import api from './util/api';
 import { AuthContext } from './AuthProvider';
@@ -24,29 +24,28 @@ import AppHeader from './Components/AppHeader';
 
 const UserSearch = () => {
     const { user } = useContext(AuthContext);
-    const [emptySearchTerm, setEmptySearchTerm] = useState(true);
+    const [searchTerm, setSearchTerm] = useState('');
     const [users, setUsers] = useState([]);
     const [usersLoading, setUsersLoading] = useState(false);
 
-    const searchUser = debounce((searchTerm) => {
-        if (searchTerm.length >= 2) {
-            setUsersLoading(true);
-            setEmptySearchTerm(false);
+    const searchUser = debounce((term) => {
+        setUsersLoading(true);
 
-            api({ token: user.token }).get('/users?search=' + searchTerm)
-                .then(({data}) => {
-                    setUsers(data.data);
-                    setUsersLoading(false);
-                })
-                .catch((error) => {
-                    setUsersLoading(false);
-                    console.error(error);
-                });
-        }
-        else if (searchTerm.length === 0) {
-            setEmptySearchTerm(true);
-        }
+        api({token: user.token}).get('/users?search=' + term)
+            .then(({data}) => {
+                setUsers(data.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+            .finally(() => {
+                setUsersLoading(false);
+            });
     }, 250);
+
+    useEffect(() => {
+        searchTerm.length >= 2 ? searchUser(searchTerm) : setUsers([]);
+    }, [searchTerm]);
 
     return (
         <View h="100%">
@@ -61,14 +60,14 @@ const UserSearch = () => {
                                 borderRadius="4"
                                 py="3" px="1"
                                 fontSize="14"
-                                onChangeText={value => searchUser(value)}
+                                onChangeText={value => setSearchTerm(value.trim())}
                                 InputLeftElement={
                                     <Icon m="2" ml="3" size="6" as={
                                         <AntDesign name="search1" size={24} color="black"/>}
                                     />}
                             />
 
-                            {usersLoading ? (
+                            { usersLoading ? (
                                 <HStack space={2} justifyContent="center">
                                     <Spinner />
                                     <Heading color="primary.500" fontSize="md">
@@ -77,19 +76,28 @@ const UserSearch = () => {
                                 </HStack>
                             ) : null }
 
-                            {!usersLoading && users.length > 0 ? (
+                            { !usersLoading && searchTerm.length >= 2 ? (
                                 <>
-                                    {users.map((user) => (
-                                        <UserCard key={ user.id } userData={ user } />
-                                    ))}
+                                    { users.length > 0 ? (
+                                        <>
+                                            { users.map((user) => (
+                                                <UserCard key={ user.id } userData={ user } />
+                                            ))}
+                                        </>
+                                    ) : (
+                                        <Center>
+                                            <Text color="primary.500">No search results.</Text>
+                                            <Text color="primary.500">Start following people to see cool posts!</Text>
+                                        </Center>
+                                    )}
                                 </>
                             ) : null }
 
-                            {!usersLoading && users.length === 0 ? (
+                            { !usersLoading && searchTerm.length <= 1 ? (
                                 <>
-                                    {emptySearchTerm ? (
+                                    { searchTerm.length === 0 ? (
                                         <Center bottom="0">
-                                            <Image style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').width }} source={require('./mushroom-muted.png')} alt='app logo'/>
+                                            <Image style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').width }} source={require('./mushroom-muted.png')} alt="app logo"/>
                                         </Center>
                                     ) : (
                                         <Center>
@@ -98,7 +106,7 @@ const UserSearch = () => {
                                         </Center>
                                     )}
                                 </>
-                            ) : null}
+                            ) : null }
 
                         </VStack>
                     </Center>
